ffc79d294960eb0ce2c15c49acc0421818f8a083
Lucas Jacques <lucas.jacs@gmail.com>
2025-05-15 16:08:23 -0300

    Add basic layout

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <br />
 <div align="center">
   <a href="https://github.com/lucasjacques/meowtube/">
-    <img src="public/logo.png" alt="Logo" width="80" height="80">
+    <img src="public/logo.svg" alt="Logo" width="80" height="80">
   </a>
 
   <h3 align="center">MeowTube</h3>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,10 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
       <div>
-        <Image src="/logo.png" height={50} width={50} alt="Logo"/>
-        <p className="text-xl font-semibold tracking-tight">
-          MeowTube2
-        </p>
+        I will load videos in the future!
       </div>
     )
 }
