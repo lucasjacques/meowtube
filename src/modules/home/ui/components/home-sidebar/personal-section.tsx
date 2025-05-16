9a490d3e9f887e1825cfdcbefbaebe82d1bf33e7
Lucas Jacques <lucas.jacs@gmail.com>
2025-05-16 15:23:22 -0300

    Add authentication

--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react"
+import { useAuth, useClerk } from "@clerk/nextjs" 
 
 import { 
     SidebarGroup,
@@ -34,6 +35,9 @@ const items = [
 ]
 
 export const PersonalSection = () => {
+    const clerk = useClerk();
+    const { isSignedIn } = useAuth();
+
     return (
         <SidebarGroup>
             <SidebarGroupLabel>You</SidebarGroupLabel>
@@ -45,7 +49,12 @@ export const PersonalSection = () => {
                                 tooltip={item.title}
                                 asChild
                                 isActive={false} // TODO: Change to look at current pathname
-                                onClick={()=>{}} // TODO: Do something on click
+                                onClick={(e)=>{
+                                    if(!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        clerk.openSignIn();
+                                    } 
+                                }}
                             >
                                 <Link href={item.url} className="flex items-center gap-4">
                                     <item.icon />
