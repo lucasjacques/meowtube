88a392d4ee04b55cda60faaa1b171cb5f600ebf7
Lucas Jacques <lucas.jacs@gmail.com>
2025-05-19 18:53:55 -0300

    Add filter carousel component

---
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { trpc } from "@/trpc/client";
+import { FilterCarousel } from "@/components/filter-carousel";
+
+interface CategoriesSectionProps {
+    categoryId?: string;
+}
+
+export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error...</p>}>
+                <CategoriesSectionSuspense categoryId={categoryId} />
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
+
+const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
+    const [ categories ] = trpc.categories.getMany.useSuspenseQuery();
+
+    const data = categories.map(({id, name}) => ({
+        value: id ,
+        label: name,
+    }));
+
+    return ( <FilterCarousel value={categoryId} data={data} />)
+}
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,11 +1,13 @@
+import { CategoriesSection } from "../sections/categories-section";
+
 interface HomeViewProps {
     categoryId?: string;
 }
 
 export const HomeView = ({ categoryId}: HomeViewProps) => {
     return (
-        <div className="max-w-[2480px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6 border bg-red-500">
-            
+        <div className="max-w-[2480px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+            <CategoriesSection categoryId={categoryId} />
         </div>
     )
 }
