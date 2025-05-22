7d88969f305f817a6b6b8ba7d0dad1cc46644376
Lucas Jacques <lucas.jacs@gmail.com>
2025-05-22 16:03:34 -0300

    Add final studio layout

--- src/modules/studio/ui/components/studio-navbar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { AuthButton } from "@/modules/auth/ui/components/auth-button";
 
+import StudioUploadModal from "../studio-upload-modal";
+
 export const StudioNavbar = () => {
     return (
         <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50 border-b shadow-md">
@@ -23,6 +25,7 @@ export const StudioNavbar = () => {
                 <div className="flex-1" />
 
                 <div className="flex-shrink-0 items-center flex gap-4">
+                    <StudioUploadModal />
                     <AuthButton />
                 </div>
             </div>
